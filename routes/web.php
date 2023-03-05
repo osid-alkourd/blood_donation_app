73bac068d5eab345bdce2ee5e9d3611933ad600f
Osid ALkourd <alkordosid73@gmail.com>
2023-03-05 22:02:11 +0200

    add asset file after update

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard' , function(){
-  return view('dashboard.table');
+  return view('dashboard.index');
 });
 
 // Route::get('/table' , function(){
