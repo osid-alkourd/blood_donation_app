2de81e88bb0a60f3ede2dbf2cf599cb6ea84951c
Osid ALkourd <alkordosid73@gmail.com>
2023-06-21 18:34:41 +0300

    Add donation appeals unit testing

---
+++ tests/Unit/DonationAppealTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Unit;
+
+//use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+class DonationAppealTest extends TestCase
+{
+    
+    // public function test_store_donation_appeal()
+    // {
+    //     $user = User::where('id' , 32)->first();
+    //     $response = $this->actingAs($user)->postJson('/api/appeals' , [
+    //         'user_id' => 32 , 
+    //         'name' => "osid2023",
+    //         'blood_type' => "A+" , 
+    //         'location' => "Dair al balah" , 
+    //         'phone_number' => '0599608424' , 
+    //         'id_number' => '111111111' ,
+    //         'description' => 'We need blood group form A+ type quickly'
+    //     ]);
+    //     $response->assertStatus(201);
+
+    // }
+
+
+    // public function test_view_donation_appeals()
+    // {
+    //     $user = User::where('id' , 32)->first();
+    //     $response = $this->actingAs($user)->getJson('api/appeals');
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_view_all_donation_appeals()
+    {
+     //   $user = User::where('id' , 32)->first();
+      $response = $this->getJson('/api/public-appeals');
+      $response->assertStatus(200);
+    }
+}
